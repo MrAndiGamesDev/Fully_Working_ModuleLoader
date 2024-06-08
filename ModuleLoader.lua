d075c1e1a99ba1f1cad33d2fab46c69e501c53d2
MrAndi Games Dev <78992535+MrAndiGamesDev@users.noreply.github.com>
2024-06-08 13:51:52 -0400

    Improved The Code it self

--- ModuleLoader.lua
+++ ModuleLoader.lua
@@ -6,113 +6,68 @@ local StarterPlayer = game:GetService("StarterPlayer")
 local StarterPlayerScripts = StarterPlayer.StarterPlayerScripts
 local Player = Players.LocalPlayer
 
+local Server = RunService:IsServer()
+local Client = RunService:IsClient()
+
+local ShortIdentifier = if Client then "[C]" else "[S]"
+
+local RootDirectory = if Server then ServerScriptService else StarterPlayerScripts or Player:WaitForChild("PlayerScripts")
+local ModuleDirectory = if Server then RootDirectory.Services else RootDirectory:WaitForChild("Controllers")
+
 local ModuleLoader = {}
 ModuleLoader.__index = ModuleLoader
 
-ModuleLoader.Server = RunService:IsServer()
-ModuleLoader.Client = RunService:IsClient()
-
-ModuleLoader.RootDirectory = if ModuleLoader.Server then ServerScriptService else StarterPlayerScripts or Player:WaitForChild("PlayerScripts")
-ModuleLoader.ModuleDirectory = if ModuleLoader.Server then ModuleLoader.RootDirectory.Services else ModuleLoader.RootDirectory:WaitForChild("Controllers")
-
 function ModuleLoader:Start()
      local self = setmetatable({}, ModuleLoader)
-     
-     self.Server = self.Server
-     self.Client = self.Client
-     
-     self.RootDirectory = self.RootDirectory
-     self.ModuleDirectory = self.ModuleDirectory
-     
-     self:RequireDescendants()
-     self:DescendantLoader()
-     self:RequireModule()
-     self:CheckLoader()
+
      self:LoadModule()
-     self:DestroyScripts()
-     self:NewPrint()
-     self:NewWarn()
-     
+
      return self
 end
 
-function ModuleLoader:NewPrint(...)
-     print(...)
-end
-
-function ModuleLoader:NewWarn(...)
-     warn(...)
-end
-
-function ModuleLoader:DescendantLoader()
-     return self.ModuleDirectory:GetDescendants()
-end
-
-function ModuleLoader:RequireModule(Module : any)
+function ModuleLoader:RequireModule(Module: ModuleScript)
      if not Module:IsA("ModuleScript") then
           return
      end
-     
-     local Import = require(Module)
-     local RunModule = Import.OnStart
 
-     if RunModule then
-          task.spawn(RunModule)
+     local AutoRequire = require(Module)
+     local Started = AutoRequire.OnStart
+
+     if Started then
+          task.spawn(Started)
      end
 end
 
-function ModuleLoader:CheckLoader()
-     for _, Descendant in self:DescendantLoader() do
-          self:RequireModule(Descendant)
-     end
+function ModuleLoader:NewPrint(...)
+     print(ShortIdentifier, ...)
 end
 
-function ModuleLoader:DestroyScripts(Module : any)
-     if Module:IsA("ModuleScript") and Module:IsA("Script") and Module:IsA("LocalScript") then
-          return
-     end
-
-     for _, Descendant in self:DescendantLoader() do
-          Descendant:Destroy()
-     end
-end
-
-function ModuleLoader:RequireDescendants()
-     self:CheckLoader()
-
-     if not self.Server and self.Client then
-          self.ModuleDirectory.DescendantAdded:Connect(function(Descendant)
-               self:RequireModule(Descendant)
-          end)
-
-          self.ModuleDirectory.DescendantRemoving:Connect(function(Descendant)
-               self:DestroyScripts(Descendant)
-          end)
-     end
-end
-
-function ModuleLoader:Calulate(...)
-     local StartTime = tick()
-     local EndTime = tick()
-     local Format = ("(took %.3f seconds)"):format(EndTime - StartTime)
-     self:NewWarn(`>> Loaded Module Took {Format} To Get Every Script {...} <<`)
+function ModuleLoader:NewWarn(...)
+     warn(ShortIdentifier, ...)
 end
 
 function ModuleLoader:LoadModule()
-     local Success, Result = pcall(function()
-          return (self or ModuleLoader) and self:RequireDescendants() and self:Start()
-     end)
-     if Success then
-          self:Calulate("Fully Loaded")
-     else
-          if not Success then
-               self:NewWarn("Failed To Run A Module Loader")
-          end
+     for _, Descendant: ModuleScript in ModuleDirectory:GetDescendants() do
+          self:NewPrint(Descendant)
+          self:RequireModule(Descendant)
+     end
+     
+     if not Server and Client then
+          ModuleDirectory.DescendantAdded:Connect(function(Descendant: ModuleScript)
+               self:RequireModule(Descendant)
+          end)
      end
 end
 
-return function ()
-     coroutine.wrap(function()
-          ModuleLoader:LoadModule()
-     end)()
+return function()
+     local Success, Result = pcall(function()
+          return ModuleLoader
+     end)
+     if Success then
+          Result:Start()
+     else
+          if not Success then
+               Result:NewWarn(`Failed To Load`)
+          end
+     end
 end
